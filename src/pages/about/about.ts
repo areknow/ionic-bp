a04811093359f74485f99797b318369b64f11755
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-07-31 15:59:53 -0400

    basic READ operation

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Observable } from 'rxjs';
+
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+
+export interface Todo {
+  description: string;
+  completed: boolean;
+}
+
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  todoCollectionRef: AngularFirestoreCollection<Todo>;
+  todo$: Observable<Todo[]>;
 
+  constructor(
+    public navCtrl: NavController,
+    private afs: AngularFirestore
+  ) {
+    this.todoCollectionRef = this.afs.collection<Todo>('todos');
+    this.todo$ = this.todoCollectionRef.valueChanges();
   }
 
 }
