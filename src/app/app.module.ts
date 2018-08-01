9a906603ddc427f17a529d5565ce95e6f559535f
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-08-01 13:39:21 -0400

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@
 // Angular + Ionic
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MyApp } from './app.component';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 // Components
+import { MyApp } from './app.component';
 import { ChartPage } from '../pages/chart/chart';
 import { ModalPage } from '../pages/modal/modal';
 
