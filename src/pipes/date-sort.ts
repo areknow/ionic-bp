ff0f56531efeb23abae22c16af048a079ce631e5
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2018-08-02 16:58:14 -0400

    reverse sorting order

--- src/pipes/date-sort.ts
+++ src/pipes/date-sort.ts
@@ -20,9 +20,9 @@ export class dateSortPipe implements PipeTransform {
     let val = value.sort((a: any, b: any) => {
       let date1 = new Date(a.date);
       let date2 = new Date(b.date);
-      if (date1 > date2) {
+      if (date1 < date2) {
         return 1;
-      } else if (date1 < date2) {
+      } else if (date1 > date2) {
         return -1;
       } else {
         return 0;
